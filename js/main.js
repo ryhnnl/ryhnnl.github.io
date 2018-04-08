9775b97b735035a82c53ece1bf77b076185f7b8e
Reyhan Ünal <rryhn.96@gmail.com>
2018-04-08 20:03:51 +0300

    add project links, remove other things

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const $ = document.querySelector.bind(document)
-
-window.addEventListener('scroll', () => {
-	if (window.scrollY < 550) {
-		$('nav').classList.remove('black')
-	} else {
-		$('nav').classList.add('black')
-	}
-})
